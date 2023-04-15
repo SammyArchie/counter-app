8ed290e981d008b5d13ae4068e79db5af7b38b09
SammyArchie <opokusammy772@gmail.com>
2023-04-15 16:10:36 +0000

    update to counter app

--- my-vue-app/src/components/CounterClass.jsx
+++ my-vue-app/src/components/CounterClass.jsx
@@ -6,12 +6,18 @@ export class CounterClass extends Component {
         this.state = {number: 0};
     }
 
+    handleIncrement = () => {
+        this.setState({ number: this.state.number + 1})
+    }
+    handleDecrement = () => {
+        this.setState({ number: this.state.number - 1})
+    };
     render() {
     return (
       <div>
-        <h1>Count: </h1>
-        <button></button>
-        <button></button>
+        <h1>Count : {this.state.number} </h1>
+        <button onClick={this.handleIncrement}>+</button>
+        <button onClick={this.handleDecrement}>-</button>
       </div>
     )
   }
